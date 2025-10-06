31745be73e14eeafa77aef8543d635074c1e2d3e
Pieter de Bruin <pdebruin@users.noreply.github.com>
2025-10-06 12:27:33 +0000

    migrating to seperate projects for dep mgmt

---
+++ inference/Program.cs
@@ -0,0 +1,49 @@
+﻿using Azure;
+using Azure.AI.Inference;
+using Microsoft.Extensions.AI;
+using ModelContextProtocol.Client;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// https://learn.microsoft.com/en-us/dotnet/ai/quickstarts/build-mcp-client
+// https://www.nuget.org/packages/Microsoft.Extensions.AI.AzureAIInference/
+// https://github.com/modelcontextprotocol/csharp-sdk/blob/main/samples/ChatWithTools/Program.cs
+IMcpClient mcpClient = await McpClientFactory.CreateAsync(
+    new SseClientTransport(new()
+    {
+        Endpoint = new Uri("https://learn.microsoft.com/api/mcp"),
+        Name = "Learn MCP Server",
+        TransportMode = HttpTransportMode.StreamableHttp,
+    }));
+
+var tools = await mcpClient.ListToolsAsync().ConfigureAwait(false);
+foreach (var tool in tools)
+{
+    Console.WriteLine($"{tool.Name}");
+}
+
+var endpoint = "https://models.github.ai/inference";
+var credential = System.Environment.GetEnvironmentVariable("GITHUB_TOKEN");
+var model = "openai/gpt-4o";
+
+if (string.IsNullOrEmpty(credential))
+    throw new InvalidOperationException("GITHUB_TOKEN environment variable is not set.");
+
+IChatClient chatClient =
+    new ChatClientBuilder(
+        new ChatCompletionsClient(new Uri(endpoint),
+        new AzureKeyCredential(credential))
+        .AsIChatClient(model))
+    .UseFunctionInvocation()
+    .Build();
+
+ChatOptions chatOptions = new()
+{
+    Tools = [.. tools] 
+};
+
+await foreach (var message in chatClient.GetStreamingResponseAsync("How to create an Azure storage account using az cli?", chatOptions))
+{
+    Console.Write(message);
+}
---
+++ semantickernel/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using ModelContextProtocol.Client;
+using System.ClientModel;
+using OpenAI;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+        // https://github.com/microsoft/semantic-kernel/blob/main/dotnet/samples/Demos/ModelContextProtocolPlugin/Program.cs
+        // MCP
+        IMcpClient mcpClient = await McpClientFactory.CreateAsync(
+            new SseClientTransport(new()
+            {
+                Endpoint = new Uri("https://learn.microsoft.com/api/mcp"),
+                Name = "Learn MCP Server",
+                TransportMode = HttpTransportMode.StreamableHttp,
+            }));
+
+        var tools = await mcpClient.ListToolsAsync().ConfigureAwait(false);
+        foreach (var tool in tools)
+        {
+            Console.WriteLine($"{tool.Name}");
+        }
+
+        // Semantic Kernel
+        var endpoint = "https://models.github.ai/inference";
+        var credential = System.Environment.GetEnvironmentVariable("GITHUB_TOKEN");
+        var model = "openai/gpt-4o";
+
+        if (string.IsNullOrWhiteSpace(credential))
+            throw new InvalidOperationException("GITHUB_TOKEN environment variable is not set.");
+
+        var client = new OpenAIClient(new ApiKeyCredential(credential), new OpenAIClientOptions { Endpoint = new Uri(endpoint) });
+        var builder = Kernel.CreateBuilder();
+        builder.AddOpenAIChatCompletion(model, client);
+        Kernel kernel = builder.Build();
+        kernel.Plugins.AddFromFunctions("LearnMcpPlugin", tools.Select(aiFunction => aiFunction.AsKernelFunction()));
+
+        OpenAIPromptExecutionSettings executionSettings = new()
+        {
+            Temperature = 0,
+            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(options: new() { RetainArgumentTypes = true })
+        };
+
+        ChatCompletionAgent agent = new()
+        {
+            Instructions = "Answer questions about Microsoft products, services and technologies",
+            Name = "LearnAgent",
+            Kernel = kernel,
+            Arguments = new KernelArguments(executionSettings),
+        };
+
+        var prompt = "How to create an Azure storage account using az cli?";
+        await foreach (var content in agent.InvokeStreamingAsync(prompt))
+        {
+            Console.Write(content.Message);
+        }
